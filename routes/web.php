6479a1f243fa9a8ae6d7f0e73747204f91c84a5d
danialfach <danialfacrudin@gmail.com>
2023-01-09 02:44:32 +0700

    Laravel redirect

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\RedirectController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get("/redirect/from", [App\Http\Controllers\RedirectController::class, "redirectFrom"]);
+
+Route::get("/redirect/to", [App\Http\Controllers\RedirectController::class, "redirectTo"]);
